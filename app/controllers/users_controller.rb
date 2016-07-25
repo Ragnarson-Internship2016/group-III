235758b21db037896e74239666eb181de98bd7d9
KamilFilip <filipowski1993@gmail.com>
2016-07-25 17:05:26 +0200

    Changed user to current user and added before_action (#67)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
+    if current_user.update(user_params)
       redirect_to root_url
     else
       render { action 'edit' }
