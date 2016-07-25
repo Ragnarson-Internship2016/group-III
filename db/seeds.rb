4d6786234582f9d00cd9699caeb44864bfbcd538
KamilFilip <filipowski1993@gmail.com>
2016-07-25 14:14:01 +0200

    Added seeds, two users and 3 events (#63)
    Credentials: lolopolo@gmail.com / lolopolo  
    test@gmail.com / lolopolo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(id: 1, name: "Kamil", surname: "Filipowski", city: "lolopolo", email: "lolopolo@gmail.com", password: "lolopolo")
+User.create(id: 2, name: "Maciej", surname: "Michalik", city: "lolopolo", email: "test@gmail.com", password: "lolopolo")
+Event.create(id: 1, user_id: 1, name: "D&D", description: "We are all playing D&D", city: "Lodz", address: "Pietryna 217", start_t: DateTime.parse("09/08/2016 17:00"), end_t: DateTime.parse("09/08/2016 22:00"))
+Event.create(id: 2, user_id: 1, name: "Grzybobranie", description: "We are all playing Grzybobranie", city: "Lodz", address: "Rojna 2", start_t: DateTime.parse("12/08/2016 10:00"), end_t: DateTime.parse("13/08/2016 10:00"))
+Event.create(id: 3, user_id: 2, name: "Pandemia", description: "We are all playing Pandemia", city: "Lodz", address: "Pietryna 272", start_t: DateTime.parse("14/09/2016 14:00"), end_t: DateTime.parse("14/09/2016 18:00"))
