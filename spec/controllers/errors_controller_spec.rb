db0bec46336f11145aa4ea80d3ad7c551ded72da
Snieezy <Snieezy@users.noreply.github.com>
2016-07-25 14:35:20 +0200

    Fix error-pages tests (#64)
    * Fix error-pages tests
    
    * Rename error-pages tests

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
   describe "GET #not_found" do
-    it "returns http success" do
+    it "returns proper http status" do
       get :not_found
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(404)
     end
   end
 
-  describe "GET #internal_server_error" do
-    it "returns http success" do
-      get :internal_server_error
-      expect(response).to have_http_status(:success)
+  describe "GET #validation_error" do
+    it "returns proper http status" do
+      get :validation_error
+      expect(response).to have_http_status(422)
     end
   end
 end
