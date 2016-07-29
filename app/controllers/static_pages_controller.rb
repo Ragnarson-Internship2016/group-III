e4a7671217b48f1776e6658de39f3bc18018c79a
hav3loc <piotr.sordyl@gmail.com>
2016-07-29 13:50:08 +0200

    Added landing page redirect (#104)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,12 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-    @skip_footer = true
-    @skip_header = true
-    render template: "layouts/landing_page"
+    if current_user
+      redirect_to events_path 
+    else
+      @skip_footer = true
+      @skip_header = true
+      render template: "layouts/landing_page"
+    end
   end
 end
